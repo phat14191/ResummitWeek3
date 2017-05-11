430a8ace923ce02ad9483c771a7d3cc827de23db
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-05-10 22:45:55 -0700

    Create output even number and Sum all even number

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 //user Interface Logic
 $(document).ready(function() {
+  console.log("ready");
   $("#userInput").submit(function(event){
-    var evenNumber = parseInt$("input#userNumber").val();
-      $("#output").appent('<li>' +  + '</li>')
-    SumAllEvenNumber(callNumber(evenNumber));
+    console.log("submit");
+    var userNumber = $("input#userNumber").val();
+    console.log("userNumber: " + userNumber);
+    var evenNumber = parseInt(userNumber);
+    console.log("evenNumber: " + evenNumber);
+    var allEvenNumbers = callNumber(evenNumber);
+    console.log(allEvenNumbers);
+    SumAllEvenNumber(allEvenNumbers);
     event.preventDefault();
-
   });
 });
 
-//Business Logic
+// Business Logic
 var input = [];
 var result = [];
 var callNumber = function(evenNumber) {
@@ -17,17 +22,21 @@ var callNumber = function(evenNumber) {
   for(i = 1; i <= evenNumber; i++) {
     if(i%2 === 0) {
       result.push(i);
+      console.log(result);
     }
   }
+  $("#output").append('<li>' + result + '</li>')
   return(result);
-  $("#output").appent('<li>' +  + '</li>')
 };
 
-  var array= ();
 
-  var SumAllEvenNumber = function(result){
+  var SumAllEvenNumber = function(array){
+    var sum = 0;
     for(i=0 ; i<array.length; i++){
-      result = result +array[i];
-
+      sum = sum +array[i];
+      console.log(sum);
     }
+    console.log(sum);
+    $("#output").append('<li>' + sum + '</li>');
+    return(sum);
   }
